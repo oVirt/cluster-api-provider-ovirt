824e4b812d232b99fa5434529090c120d0832f28
Roy Golan <rgolan@redhat.com>
2020-03-16 20:29:56 +0200

    Use the CA bundle from the credentials when possible
    oVirt API over SSL should be verified against a proper CA. It is
    expected by the deployment to pass the CA bundle that container oVirt's
    CA and keep it in ovirt-credentials secret, under 'ovirt_ca_bundle'.
    
    When it's there, save it to disk, and use it as the api connection CA
    file.
    
    Another option was to seriailize the secret and extract the CA bundle
    directly to /etc/ssl/certs but that mandates a deployment change, and
    moving that logic to the deployment part.
    
    If the CA bundle doesn't exist the deploymnent can still handle that
    by changing the CA sources on the container.
    
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- pkg/cloud/ovirt/clients/credentials.go
+++ pkg/cloud/ovirt/clients/credentials.go
@@ -8,10 +8,14 @@ package clients
 import (
 	"context"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"strconv"
+	"strings"
 
 	apicorev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/klog"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -21,6 +25,7 @@ type OvirtCreds struct {
 	Password string
 	CAFile   string
 	Insecure bool
+	CABundle string
 }
 
 func GetCredentialsSecret(coreClient client.Client, namespace string, secretName string) (*OvirtCreds, error) {
@@ -39,11 +44,39 @@ func GetCredentialsSecret(coreClient client.Client, namespace string, secretName
 	o.Username = string(credentialsSecret.Data["ovirt_username"])
 	o.Password = string(credentialsSecret.Data["ovirt_password"])
 	o.CAFile = string(credentialsSecret.Data["ovirt_cafile"])
-	insecure, err  := strconv.ParseBool(string(credentialsSecret.Data["ovirt_insecure"]))
+	insecure, err := strconv.ParseBool(string(credentialsSecret.Data["ovirt_insecure"]))
 	if err == nil {
 		o.Insecure = insecure
 	}
+	o.CABundle = string(credentialsSecret.Data["ovirt_ca_bundle"])
 
+	// write CA bundle to a file if exist.
+	// its best if we could mount the secret into a file,
+	// but this controller deployment cannot
+	if o.CABundle != "" {
+		caFilePath, err := writeCA(strings.NewReader(o.CABundle))
+		if err != nil {
+			klog.Errorf("failed to extract and store the CA %s", err)
+			return nil, err
+		}
+		o.CAFile = caFilePath
+	}
 	return &o, nil
 }
 
+func writeCA(source io.Reader) (string, error) {
+	f, err := ioutil.TempFile("", "ovirt-ca-bundle")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	content, err := ioutil.ReadAll(source)
+	if err != nil {
+		return "", err
+	}
+	_, err = f.Write(content)
+	if err != nil {
+		return "", err
+	}
+	return f.Name(), nil
+}
